04f194af6a106f502fcb001a0bb0893d3d03ad04
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-01-18 15:06:52 -0300

    add theme in app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { useState } from 'react';
-
+import { NativeBaseProvider, Spinner } from 'native-base';
+import {THEME} from './src/theme';
+import { Loading } from '@components/Loading';
 export default function App() {
+
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
-  const [name, setName] = useState('Rafael')
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#202024' }}>
+  
+    return (
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
-      />
-      {
-        fontsLoaded ? <Text>Hello, {name} </Text> : <View />
-      }
-    </View>
+        />
+        {
+          fontsLoaded ? <View /> : <Loading />
+        }
+      
+    </NativeBaseProvider>
   );
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import { Center, Spinner, View } from "native-base";
+
+export function Loading() {
+  return (
+    <Center flex={1} bg="gray.700">
+      <Spinner color="gray.500" />
+    </Center>
+
+  )
+}
